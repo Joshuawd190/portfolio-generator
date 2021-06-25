5dc9c941fb3503be377b28a847f311fdd0b9d8fe
Joshua D Wood <joshuawd190@gmail.com>
2021-06-24 19:54:33 -0500

    Added conditionals to about question

--- app.js
+++ app.js
@@ -32,9 +32,16 @@ const promptUser = () =>{
       validate: gitInput => gitInput ? true : console.log('Please enter your GitHub username!')
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      when: ({confirmAbout}) => confirmAbout ? true : false
     }
   ]);
 };
